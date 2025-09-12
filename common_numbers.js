c5d67011161ad4509110f7a85430760750e7c0b3
Shanmugapriya-numentica <shanmugapriya.murugan@numentica-ui.com>
2025-09-12 15:30:10 +0530

    Update common_numbers.js

--- common_numbers.js
+++ common_numbers.js
@@ -16,7 +16,7 @@ function findCommonNumbers(firstSubArray, secondSubArray) {
 
         if (typeof firstSubArray[i] !== 'number') {
             console.error("Array should only contain numbers");
-            return "Input Invalid";
+            return [];
         }
 
         let firstArrayValue = firstSubArray[i];
@@ -24,7 +24,8 @@ function findCommonNumbers(firstSubArray, secondSubArray) {
             let secondArrayValue = secondSubArray[j];
 
             if (typeof secondSubArray[j] !== 'number') {
-                return 'invalid'
+                console.log("Input Invalid");
+                return [];
             }
 
             if (firstArrayValue === secondArrayValue) {
@@ -55,3 +56,4 @@ console.log(findCommonNumbers([3, 45, 42, -7, 11, 34], [35, -7, 87, 11, 1, 45]))
 // console.log(findCommonNumbers(["a","b","c","d"], ["a",'c']));
 // console.log(findCommonNumbers([1, 2, 3, 6, 4, 4, 5, 2], [1, 5, 5, 3, 4]));
 // console.log(findCommonNumbers([3, 45, 42, -7, 11, 34],null))
+
